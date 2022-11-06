fad2af3fb76fdc45e80ec638143bb85eaf21a592
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-11-06 15:00:51 +0900

    set configs in progress...

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './build') 
+  },
+  devtool: 'eval-cheap-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // 로더가 2개 이상일 때 해당 파일에 적용할 로더의 배열, 오른쪽부터 왼쪽 방향 순서
+      },
+      {
+        test: /\.jfif$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        }
+      },
+    ],
+  },
+// webpack-dev-server options
+  devServer: {
+    hot: true,
+    port: 3000,
+    // overlay: true,
+    // writeToDisk: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
