2e9d07f7010768a700928f75e43cd5c9bdb8f093
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-11-06 21:23:41 +0900

    Modify Index preferred for React18

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./containers/App";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './containers/App'
 
-const rootElement = document.getElementById("app");
-
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, rootElement);
+const container = document.getElementById('app')
+const root = createRoot(container!) // createRoot(container!) if you use TypeScript
+root.render(
+  <>
+    <App />
+  </>,
+)
