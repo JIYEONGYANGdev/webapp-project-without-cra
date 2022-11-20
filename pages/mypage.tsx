30f1038dca250a060e55f1f54da40e08c142e1dc
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-11-21 00:52:09 +0900

    fix Header component's width over html (safari in case yet)

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,81 +1,17 @@
+import Head from 'next/head'
 import React from 'react'
-import styled from 'styled-components'
-import styles from './Mypage.module.scss'
+import Mypage from '~/containers/Mypage'
 
 const mypage = () => {
   return (
     <>
-      <Header />
+      <Head>
+        <title>마이페이지</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, viewport-fit=cover" />
+      </Head>
+      <Mypage />
     </>
   )
 }
-
-const Header = () => {
-  return (
-    <Wrapper>
-      <LogoWrapper>
-        <img src={'/busiman_logo.png'} alt="메인 홈 로고" />
-        <Title>영화제 시간표 만들어 드립니다.</Title>
-      </LogoWrapper>
-      <IconWrppaer>
-        <i className="fa-solid fa-magnifying-glass" />
-        <i className="fa-solid fa-bars" />
-      </IconWrppaer>
-    </Wrapper>
-  )
-}
-
-const Wrapper = styled.div`
-  width: 100%;
-  max-height: 54px;
-
-  display: flex;
-  justify-content: space-between;
-
-  padding: 10px 20px;
-  background-color: #fff4ce;
-
-  @media screen and (min-width: 769px) {
-    max-height: 92px;
-    padding: 16px 58px;
-  }
-`
-
-const LogoWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-
-  img {
-    height: 42px;
-    padding-right: 8px;
-
-    @media screen and (min-width: 769px) {
-      height: 64px;
-      padding-right: 12px;
-    }
-  }
-`
-
-const Title = styled.div`
-  font-family: 'HSWinterNoonkott2_0';
-  font-weight: 700;
-  font-size: 15px;
-
-  @media screen and (min-width: 769px) {
-    font-size: 24px;
-  }
-`
-
-const IconWrppaer = styled.div`
-  i {
-    font-size: 20px;
-    padding: 10px;
-
-    :last-of-type {
-      padding-right: 0;
-    }
-  }
-`
 
 export default mypage
